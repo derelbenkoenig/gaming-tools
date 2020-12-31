7b3dab705ad463fc710d654e4d49b5b41a72ea88
derelbenkoenig <derelbenkoenig@gmail.com>
2020-12-31 15:38:47 -0500

    comment about s values

--- ssbm/Ssbm.hs
+++ ssbm/Ssbm.hs
@@ -1,5 +1,7 @@
 module Ssbm where
 
+-- Fully depressed trigger corresponds with s = 1.0, 
+-- while the lightest possible analog press that still yields shield has s = 0.30714.
 shieldstun :: Double -> Double -> Int
 shieldstun damage triggerLevel =
     floor $ (200.0 / 201.0) * (((d * (a + 0.3) * 1.5) + 2.0))
